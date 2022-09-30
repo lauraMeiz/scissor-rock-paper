acd2e588fa9ed5efd09aa496fde7dcb33a0124a4
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-09-30 10:07:32 +0300

    result initial

--- src/Components/game.jsx
+++ src/Components/game.jsx
@@ -8,19 +8,42 @@ export default function Game() {
   const [choice, setChoice] = useState([scissor, rock, paper]);
   const [xChoice, setXchoice] = useState([]);
   const [computer, setComputer] = useState([]);
+  const [humainResult, setHumainResult] = useState(0);
+  const [computerResult, setComputerResult] = useState(0);
+
   const letChoice = (choic) => {
     setXchoice(choic);
-    console.log(choic);
+
     computerChoice();
   };
-  const computerChoice = () => {
+  const computerChoice = (i) => {
     const randomNumber = Math.floor(Math.random() * 3);
     //  return randomNumber
     const comp = choice[randomNumber];
-    console.log(choice[randomNumber]);
+
     setComputer(comp);
-    console.log(computer);
+
+    if (computer === paper && xChoice === scissor) {
+      setHumainResult((humainResult) => (humainResult += 1));
+      setComputerResult(computerResult);
+    }
   };
+
+  // const points = () => {
+  //   let sum = 0;
+  //   let move = 0;
+  //   if (computer === paper && xChoice === scissor) {
+  //     move++;
+  //     sum += move;
+  //   }
+  //   setResult(sum);
+  //   console.log(result);
+  // };
+
+  // console.log(xChoice === paper);
+  // console.log(computer === xChoice);
+  // console.log(xChoice);
+  // console.log(move);
 
   return (
     <>
@@ -41,18 +64,24 @@ export default function Game() {
           ))}
         </div>
 
-        <div className="right-column"></div>
+        <div className="right-column">
+          <div className="game-result">{humainResult}</div>
+          <div className="game-result">{computerResult}</div>
+        </div>
       </div>
-      <div>
-        pasirinkimas<img src={xChoice} alt={xChoice}></img>
+
+      <div className="game-row">
+        <div>
+          pasirinkimas<img src={xChoice} alt={xChoice}></img>
+        </div>
+        <div>
+          kompas<img src={computer} alt={computer}></img>
+        </div>
       </div>
-      <div>
-        kompas<img src={computer} alt={computer}></img>
-      </div>
-      <div className="bottom-row">
+      {/* <div className="bottom-row">
         <div>Result</div>
         <div className="game-result"></div>
-      </div>
+      </div> */}
     </>
   );
 }
