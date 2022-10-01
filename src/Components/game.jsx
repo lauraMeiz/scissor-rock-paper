fbb1265528bb72bfe115fb0e5418420e108802a9
Laura <97021990+lauraMeiz@users.noreply.github.com>
2022-10-01 17:27:18 +0300

    useEffect commit

--- src/Components/game.jsx
+++ src/Components/game.jsx
@@ -2,7 +2,7 @@ import scissor from "../images/scissor.jpg";
 import rock from "../images/rock.jpg";
 import paper from "../images/paper.jpg";
 import what from "../images/what.jpg";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useMemo, useState } from "react";
 import Choice from "./Choice";
 export default function Game() {
   const [choice, setChoice] = useState([scissor, rock, paper]);
@@ -11,55 +11,79 @@ export default function Game() {
   const [humainResult, setHumainResult] = useState(0);
   const [computerResult, setComputerResult] = useState(0);
   const [clicked, setClicked] = useState(0);
+  const [phrase, setPhrase] = useState("");
 
   const letChoice = (choic) => {
     setXchoice(choic);
 
     computerChoice();
+    checkWinner();
     setClicked(1);
   };
-  const computerChoice = (i) => {
+
+  const computerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
-    //  return randomNumber
+
     const comp = choice[randomNumber];
 
     setComputer(comp);
+  };
 
+  const checkWinner = () => {
     if (computer === paper && xChoice === scissor) {
       setHumainResult((humainResult) => (humainResult += 1));
       setComputerResult(computerResult);
+      setPhrase((phrase) => (phrase = "You win"));
     }
     if (computer === scissor && xChoice === paper) {
       setHumainResult(humainResult);
       setComputerResult((computerResult) => (computerResult += 1));
+      setPhrase((phrase) => (phrase = "Computer win"));
     }
     if (computer === paper && xChoice === rock) {
       setHumainResult(humainResult);
       setComputerResult((computerResult) => (computerResult += 1));
+      setPhrase((phrase) => (phrase = "Computer win"));
     }
     if (computer === rock && xChoice === paper) {
-      setHumainResult((computerResult) => (computerResult += 1));
+      setHumainResult((humainResult) => (humainResult += 1));
       setComputerResult(computerResult);
+      setPhrase((phrase) => (phrase = "You win"));
     }
     if (computer === scissor && xChoice === rock) {
       setHumainResult((humainResult) => (humainResult += 1));
       setComputerResult(computerResult);
+      setPhrase((phrase) => (phrase = "You win"));
     }
     if (computer === rock && xChoice === scissor) {
       setHumainResult(humainResult);
       setComputerResult((computerResult) => (computerResult += 1));
+      setPhrase((phrase) => (phrase = "Computer win"));
     }
-    // if (computer === scissor && xChoice === paper) {
-    //   setHumainResult(humainResult);
-    //   setComputerResult((computerResult) => (computerResult += 1));
-    // }
+    if (computer === rock && xChoice === rock) {
+      setPhrase((phrase) => (phrase = "Nobody"));
+    }
+    if (computer === scissor && xChoice === scissor) {
+      setPhrase((phrase) => (phrase = "Nobody"));
+    }
+    if (computer === paper && xChoice === paper) {
+      setPhrase((phrase) => (phrase = "Nobody"));
+    }
+    return;
   };
+  useEffect(() => {
+    checkWinner();
+  }, [xChoice, computer]);
 
   return (
     <>
       <div className="top-row">
         <div>
           <h1>Rock Paper Scissors</h1>
+        </div>
+        <div className="second-row">
+          <div>You</div>
+          <div> Computer</div>
         </div>
         <div className="result-column">
           <div className="game-result"> {humainResult}</div>
@@ -93,6 +117,7 @@ export default function Game() {
           )}
         </div>
       </div>
+      <div>{phrase}</div>
     </>
   );
 }
